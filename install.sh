4bcfd5c0f268b35806ccb72318987e7042c2ce6f
MININT-EPVG1V8-local <MININT-EPVG1V8-local@MININT-EPVG1V8>
2021-11-18 22:58:59 +0700

    Update Amazone Linux

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PLAIN='\033[0m'
 
-#My user and passs
+#User and key
 MYUSER='sonnh11'
 MYPASSWD='123123'
 MYSSHKEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8mhLODwa7qWIOnm1uEmuq4788GMTCX2kcFgCB/UMD87Vm0wtqaBGSy9EwpfUbaWifHtXH+P3JY0vNgxAibQn2j4PDHILkg9zrR81FzTcBCPeBvc+vEqlNWCTtBWAGb19WMMNzfj7DMFxP6aV2H9pgHUkiHhFLOyyC1WnGjeusl6j9lt+9s9G0BOQ0iPFLkRoWFZjYbSBOa1CNJTTQFB+tRh44M2nXhkam3Zn0GtMD27T5jwz6a+8NwhKE1M2uoFCWvTNX0t/R72DkPe3ztjq8Zj5/sL+4E3BLX+OK9eQf1/10v2iDWqGeICx6WwUYlIesr9a4P2Vx7p0usd+cjAHF sonnh11@server1'
@@ -33,7 +33,7 @@ checkOSInfo() {
             "Ubuntu")
                 OS_FAMILY=1
                 ;;
-            "CentOS Linux"|"Red Hat Enterprise Linux")
+            "CentOS Linux"|"Red Hat Enterprise Linux"|"Amazon Linux"")
                 OS_FAMILY=2
                 ;;
             *)
@@ -42,11 +42,12 @@ checkOSInfo() {
         esac
     else
         printf "${RED}Error: ${PLAIN} Cannot check OS information\n"
+        exit 1
     fi
 
 }
 
-# sudo without password
+# Add my user to system
 addnewUser() {
     if [[ $OS_FAMILY -eq 1 ]]; then
         adduser --disabled-password --gecos "" $MYUSER
@@ -102,8 +103,6 @@ installZSH () {
         sed -i "/root\/.oh-my-zsh/c\export ZSH=\"/home/$MYUSER/.oh-my-zsh\"" /home/$MYUSER/.zshrc
     fi
 }
-
-
 
 main() {
     addnewUser
