dcf2392c08ad99632d51f6eb7d37cb4faea223e1
hecma002 <sonnh.uit@gmail.com>
2023-05-20 16:24:03 +0700

    update output

--- terraform/aws/modules/iams/outputs.tf
+++ terraform/aws/modules/iams/outputs.tf
@@ -1,22 +1,22 @@
 output "account_id" {
-    value = data.aws_caller_identity.account_id.account_id
-  
+  value = data.aws_caller_identity.account_id.account_id
+
 }
+
 output "authenticate_list_account" {
-    # type = map
-    value = [
+  value = [
+    {
+      account = [
+        for idx in range(length(var.ops_list_username)) :
         {
-            account = [
-                {
-                    "username" : {for user, i in aws_iam_user.create_ops_account: user => i.name},
-                    "password" : {for pass, i in aws_iam_user_login_profile.ops_account_password: pass => i.password},
-                    "access_key" : {for key, i in aws_iam_access_key.ops_account_access_keys: key => i.id},
-                    "secret_access": {for secret, i in aws_iam_access_key.ops_account_access_keys: secret => i.secret}
-                }
-               
-            ]
+            "username" : aws_iam_user_login_profile.ops_account_password[idx].password,
+            "password" : aws_iam_user.create_ops_account[idx].name,
+            "access_key" : aws_iam_access_key.ops_account_access_keys[idx].id,
+            "access_secret" : aws_iam_access_key.ops_account_access_keys[idx].secret,
+
         }
-        
-    ] 
-        
+      ]
+    }
+  ]
+
 }
--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,14 +1,14 @@
-# output "instance_public_ip" {
-#     description = "The public IP of instace"
-#     value = module.instances.public_ip
+output "instance_public_ip" {
+    description = "The public IP of instace"
+    value = module.instances.public_ip
   
-# }
+}
 
-# output "instance_private_ip" {
-#     description = "The public IP of instace"
-#     value = module.instances.private_ip
+output "instance_private_ip" {
+    description = "The public IP of instace"
+    value = module.instances.private_ip
   
-# }
+}
 
 output "account_id" {
     value = module.iam.account_id
